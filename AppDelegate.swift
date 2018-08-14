3d656207d1f8490b182d5d66c12278ebda50a6e1
Mohamed Arafa <38532240+arafa93@users.noreply.github.com>
2018-08-14 07:14:08 -0700

    Update AppDelegate.swift

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -18,17 +18,106 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    
+    let AcceptedAndDoneVc = AcceptedAndDoneVC()
+    let RecieveInvoiceVc = RecieveInvoiceVC()
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+   
+        // For debugging
+        //OneSignal.setLogLevel(.LL_VERBOSE, visualLevel: .LL_NONE)
         
+        
+        // -- Mark OutPut
+        /*
+             Received Notification: Optional("f8331601-c549-4332-9b36-50d9456c2c0b")
+             launchURL = None
+             content_available = true
+             Message = Optional("Tech Confirm your order")
+             badge number = 0
+             notification sound = default
+             additionalData = [AnyHashable("identifier"): RecieveInvoiceVc]
+             actionSelected = []
+         */
+
+        
+        let notificationReceivedBlock: OSHandleNotificationReceivedBlock = { notification in
+            
+            print("Received Notification: \(notification!.payload.notificationID)")
+            print("launchURL = \(notification?.payload.launchURL ?? "None")")
+            print("content_available = \(notification?.payload.contentAvailable ?? false)")
+        }
+
+        let notificationOpenedBlock: OSHandleNotificationActionBlock = { result in
+            // This block gets called when the user reacts to a notification received
+            let payload: OSNotificationPayload? = result?.notification.payload
+            
+            print("Message = \(payload!.body)")
+            print("badge number = \(payload?.badge ?? 0)")
+            print("notification sound = \(payload?.sound ?? "None")")
+            
+            if let additionalData = result!.notification.payload!.additionalData {
+                print("additionalData = \(additionalData)")
+                
+                
+                if let actionSelected = payload?.actionButtons {
+                    print("actionSelected = \(actionSelected)")
+                }
+                
+                // DEEP LINK from action buttons
+                if let actionID = result?.action.actionID {
+                    
+                    // For presenting a ViewController from push notification action button
+                    let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                    let instantiateRedViewController : UIViewController = mainStoryboard.instantiateViewController(withIdentifier: "AcceptedAndDoneVc") as UIViewController
+                    let instantiatedGreenViewController: UIViewController = mainStoryboard.instantiateViewController(withIdentifier: "RecieveInvoiceVc") as UIViewController
+                    self.window = UIWindow(frame: UIScreen.main.bounds)
+                    
+                    print("actionID = \(actionID)")
+                    
+                    // This block gets called when the user reacts to a notification received
+                    let payload: OSNotificationPayload = result!.notification.payload
+                    
+                    print("Payload contains all of properties in notif is : " , payload)
+                    print("Payload additionalData : " , payload.additionalData)
+                    
+                    let when = DispatchTime.now() + 2
+                    DispatchQueue.main.asyncAfter(deadline: when) {
+                        
+                        //Go to messages
+                        NotificationCenter.default.post(name: Notification.Name("AcceptedAndDoneVc"), object: nil)
+                        
+                        
+                    }
+                    
+                    
+                    if actionID == "id2" {
+                        print("do something when button 2 is pressed")
+                        self.window?.rootViewController = instantiateRedViewController
+                        self.window?.makeKeyAndVisible()
+                        
+                        
+                    } else if actionID == "id1" {
+                        print("do something when button 1 is pressed")
+                        self.window?.rootViewController = instantiatedGreenViewController
+                        self.window?.makeKeyAndVisible()
+                        
+                    }
+                }
+            }
+        }
+
         //for Push Notifications
-        let onesignalInitSettings = [kOSSettingsKeyAutoPrompt: false]
+        let onesignalInitSettings = [kOSSettingsKeyAutoPrompt: false, kOSSettingsKeyInAppLaunchURL: true, ]
+        
+        // Add your AppDelegate as an obsserver
+        //OneSignal.add(self as! OSPermissionObserver)
+        
+        //OneSignal.add(self as! OSSubscriptionObserver)
         
         // Replace 'YOUR_APP_ID' with your OneSignal App ID.
-        OneSignal.initWithLaunchOptions(launchOptions,
-                                        appId: "67b34326-6383-497f-86a4-fb08d3d0e628",
-                                        handleNotificationAction: nil,
-                                        settings: onesignalInitSettings)
+        OneSignal.initWithLaunchOptions(launchOptions, appId: "67b34326-6383-497f-86a4-fb08d3d0e628", handleNotificationReceived: notificationReceivedBlock, handleNotificationAction: notificationOpenedBlock, settings: onesignalInitSettings)
         
         OneSignal.inFocusDisplayType = OSNotificationDisplayType.notification;
         
@@ -41,6 +130,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    // Add this new method
+    func onOSPermissionChanged(_ stateChanges: OSPermissionStateChanges!) {
+        
+        // Example of detecting answering the permission prompt
+        if stateChanges.from.status == OSNotificationPermission.notDetermined {
+            if stateChanges.to.status == OSNotificationPermission.authorized {
+                print("Thanks for accepting notifications!")
+            } else if stateChanges.to.status == OSNotificationPermission.denied {
+                print("Notifications not accepted. You can turn them on later under your iOS settings.")
+            }
+        }
+        // prints out all properties
+        print("PermissionStateChanges: \n\(stateChanges)")
+    }
+    
+
+    // TODO: update docs to change method name
+    // Add this new method
+    func onOSSubscriptionChanged(_ stateChanges: OSSubscriptionStateChanges!) {
+        if !stateChanges.from.subscribed && stateChanges.to.subscribed {
+            print("Subscribed for OneSignal push notifications!")
+        }
+        print("SubscriptionStateChange: \n\(stateChanges)")
+    }
+    
+    // Output:
+    
+    /*
+         Message = Optional("Customer waiting your response on his order")
+         badge number = 0
+         notification sound = default
+         additionalData = [AnyHashable("identifier"): RecieveInvoiceVc]
+         actionSelected = []
+     */
+    
+    
+
     
 
 
